10085d950f2ce2641691dfad57b39f003e89be9c
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2021-08-18 14:53:38 -0400

    plot k means clusters
    created graphs for k means clustering, and removed more variables based on PCA

--- Beans_kMeanClusters.R
+++ Beans_kMeanClusters.R
@@ -1,5 +1,5 @@
-#setwd("~/GitHub/Beans/WD")
-setwd("C:/Users/mortimerk1/Desktop/Year 3/Summer 2021/Machine Learning/Final Project/Beans/WD")
+setwd("~/GitHub/Beans/WD")
+#setwd("C:/Users/mortimerk1/Desktop/Year 3/Summer 2021/Machine Learning/Final Project/Beans/WD")
 
 #data = read.csv('subsettedBeansNoColumns.csv', as.is = T)
 data = read.csv('FullBeans2.csv', as.is = T)
@@ -7,22 +7,22 @@ data = read.csv('FullBeans2.csv', as.is = T)
 
 data = data[,-which(names(data) %in% c("Extent","Solidity"))]
 
-B<- data[,1]
+B<- data[,1] #Area
 C<-data$Perimeter
 D<-data$MajorAxisLength
 E<-data$MinorAxisLength
-F<-data$AspectRation
-G<-data$Eccentricity
+#F<-data$AspectRation
+#G<-data$Eccentricity
 H<-data$ConvexArea
 I<-data$EquivDiameter
 #J <- data$Extent
 #K <- data$Solidity
-L <- data$roundness
-M <- data$Compactness
+#L <- data$roundness
+#M <- data$Compactness
 N <- data$ShapeFactor1
-O <- data$ShapeFactor1
-P <- data$ShapeFactor2
-Q <- data$ShapeFactor4
+#O <- data$ShapeFactor1
+#P <- data$ShapeFactor2
+#Q <- data$ShapeFactor4
 
 
 #normalize data
@@ -42,13 +42,13 @@ meanE <- mean(E)
 stE <- sd(E)
 E<- (E-meanE )/stE
 
-meanF <- mean(F)
-stF <- sd(F)
-F<- (F-meanF )/stF
-
-meanG <- mean(G)
-stG <- sd(G)
-G<- (G-meanG )/stG
+# meanF <- mean(F)
+# stF <- sd(F)
+# F<- (F-meanF )/stF
+# 
+# meanG <- mean(G)
+# stG <- sd(G)
+# G<- (G-meanG )/stG
 
 meanH <- mean(H)
 stH <- sd(H)
@@ -65,41 +65,51 @@ I <- (I-meanI )/stI
 # meanK <- mean(K)
 # sdK <- sd(K)
 # K <-(K-meanK)/sdK
-
-meanL <- mean(L)
-sdL <- sd(L)
-L <-(L-meanL)/sdL
-
-meanM <- mean(M)
-sdM <- sd(M)
-M <-(M-meanM)/sdM
+# 
+# meanL <- mean(L)
+# sdL <- sd(L)
+# L <-(L-meanL)/sdL
+# 
+# meanM <- mean(M)
+# sdM <- sd(M)
+# M <-(M-meanM)/sdM
 
 meanN <- mean(N)
 sdN <- sd(N)
 N <-(N-meanN)/sdN
 
-meanO <- mean(O)
-sdO <- sd(O)
-O <-(O-meanO)/sdO
-
-meanP <- mean(P)
-sdP <- sd(P)
-P <-(P-meanP)/sdP
-
-meanQ <- mean(Q)
-sdQ <- sd(Q)
-Q <-(P-meanQ)/sdQ
+# meanO <- mean(O)
+# sdO <- sd(O)
+# O <-(O-meanO)/sdO
+# 
+# meanP <- mean(P)
+# sdP <- sd(P)
+# P <-(P-meanP)/sdP
+# 
+# meanQ <- mean(Q)
+# sdQ <- sd(Q)
+# Q <-(P-meanQ)/sdQ
 
 #data1 <- data.frame(B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q)
 #colnames(data1) <- c("B","C","D","E","F","G","H",'I',"J","K","L","M","N","O","P","Q")
+# 
+# data2 <- data.frame(B,C,D,E,F,G,H,I,L,M,N,O,P,Q)
+# colnames(data2) <- c("B","C","D","E","F","G","H",'I',"L","M","N","O","P","Q")
 
-data2 <- data.frame(B,C,D,E,F,G,H,I,L,M,N,O,P,Q)
-colnames(data2) <- c("B","C","D","E","F","G","H",'I',"L","M","N","O","P","Q")
+data3 <- data.frame(B,C,D,H,I,N)
+colnames(data3) <- c("B","C","D","H",'I',"N")
 
-clusters = kmeans(data2, 7, nstart = 7, algorithm = "Lloyd", iter.max = 1000)
+clusters = kmeans(data3, 7, nstart = 7, algorithm = "Lloyd", iter.max = 1000)
 
 centers = clusters$centers
 
 print(centers)
 
 centers2 <- data.frame(centers)
+
+
+plot(centers[,1],centers[,2])
+plot(centers[,1],centers[,3])
+plot(centers[,1],centers[,4])
+plot(centers[,1],centers[,5])
+plot(centers[,1],centers[,6])
